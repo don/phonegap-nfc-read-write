17866e909ba139d0e586c691d176ce33f9827af6
Don Coleman <dcoleman@chariotsolutions.com>
2016-06-14 12:48:09 -0400

    cleanup

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var readCount = 0;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,18 +17,53 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+
+        // Read NDEF formatted NFC Tags
+        nfc.addNdefListener (
+            app.onNdef,
+            function () { // success callback
+                app.log('Ready. Please scan a NFC tag.\n');
+            },
+            function (error) { // error callback
+                navigator.notification.alert('Error adding NDEF listener ' + JSON.stringify(error));
+            }
+        );
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    onNdef: function (nfcEvent) {
+        var tag = nfcEvent.tag,
+            ndefMessage = tag.ndefMessage;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // log the raw json of the message
+        console.log(JSON.stringify(ndefMessage));
 
-        console.log('Received Event: ' + id);
+        // assuming the first record in the message has
+        // a payload that can be converted to a string.
+        // real apps should read the TNF, type, and leading bytes of payload
+        // Hack for demo: removing first 3 bytes, assuming [2, 'e', 'n']
+        var text = nfc.bytesToString(ndefMessage[0].payload).substring(3);
+        app.log('NFC tag contains: ' + text);
+
+        // increment the read count and write the counter value to the tag
+        readCount++;
+        var message = [
+            ndef.textRecord(readCount)
+        ];
+
+        nfc.write(
+            message,
+            function() {
+                app.log('\nWrote "' + readCount + '" to the NFC tag.\nScan again to increase the count.\n');
+            },
+            function() {
+                navigator.notification.alert('Failed to write "' + readCount + '" to the NFC tag', null, 'ERROR');
+            }
+        );
+    },
+    // log message to the console and HTML
+    log: function(message) {
+        console.log(message);
+        var pre = document.querySelector('pre');
+        pre.innerHTML += '\n' + message;
     }
 };
 
